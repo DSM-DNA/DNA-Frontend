554901409d0ffebfc04112264e51187c1302ce04
pjp0704 <junhyeong25@gmail.com>
2021-08-20 01:53:10 +0900

    <Style> unused import statements

--- src/containers/auth/LoginForm.tsx
+++ src/containers/auth/LoginForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import AuthForm from '../../components/auth/AuthForm';
 import { useUserState, useUserDispatch } from '../../contexts/user';
--- src/containers/auth/RegisterForm.tsx
+++ src/containers/auth/RegisterForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useHistory } from 'react-router-dom';
 import AuthForm from '../../components/auth/AuthForm';
 import { useUserState, useUserDispatch } from '../../contexts/user';
